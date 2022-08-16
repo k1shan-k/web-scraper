eb840384a2501a77cb4f709634d152429ccd5955
kishan <kishankumardwivedi0@gmail.com>
2022-08-16 22:18:45 +0530

    added test file

--- run.py
+++ run.py
@@ -1,8 +1,117 @@
-print("kishan")
+# # # # from datetime import date
+# # # #
+# # # # letter = '''Dear, <|name|>
+# # # # You are selected
+# # # # date: <|Date|>'''
+# # # #
+# # # # a = input("what is your name ?\n ")
+# # # #
+# # # # b = str(date.today())
+# # # #
+# # # # letter = letter.replace("<|name|>", a)
+# # # # letter = letter.replace("<|Date|>", b)
+# # # #
+# # # # print(letter)
+# # #
+# # # test = "bro wtf are you talking  about"
+# # #
+# # # piss = test.replace("  " , " ")
+# # #
+# # # print(piss)
+# #
+# # a = min(2,54,56)
+# # #b = max[34,3453,54]
+# #
+# # print(a)
+# # #print(b)
+# #
+# a=[]
+# for i  in range(6):
+#     l=int(input("please enter the marks of student"))
+#     a.append(l)
+# a.sort()
+# print(a)
 
-x = input("what is your name")
 
-if x == 'kishan':
-    print("hacker the god")
-else:
-    print(x)
+# import requests
+# a=[]
+# for i in range(2):
+#     test = requests.get("https://ipinfo.io")
+#     real = test.json()
+#     a.append(real['ip'])
+# print(a)
+
+
+# list = [23,44,43]
+#
+# list.sort()
+#
+# print(list)
+# m = {
+#     "kishan" : "greatest haxxor",
+#      "avnish" : "head of ceo"
+# }
+#
+# print(m["kishan"])
+
+
+#
+# a = int(input("any random value please:"))
+# if(a<4):
+#     print("we are talking about 4")
+# elif(a>50):
+#     print("we are talking about 50")
+#
+# else:
+#     print("nigger")
+
+
+# a = int(input("Please enter your age : "))
+#
+# if (a>=18 or a<60):
+#     print("welcome to bar sir")
+# else:
+#     print("kido")
+#
+#
+# import requests
+#
+# kishan = requests.get("https://ipinfo.io")
+#
+# print(kishan.json())
+
+# text = input("enter your comment please: ")
+#
+# spam = False
+#
+# if ("money the god" in text):
+#     spam = True
+# elif ("buy now" in text):
+#     spam = True
+# else:
+#     print("you are sexy")
+#
+# a = input("What is your username: \n")
+#
+# b = len(a)
+#
+# if (b<=10):
+#
+#     print("please enter username (must be more then 10) :\n")
+# else:
+#     print("bro you are god")
+# #print(b)
+#
+# a = [ "kishan", "dada", "me"]
+#
+# b = input("please enter any name")
+#
+# if (b in a):
+#     print("yes is in the list")
+# else:
+#     print("americans go go go")
+
+a = int(input("please enter the marks of kishan: \n"))
+
+if (a>90<100):
+    print("you ")
