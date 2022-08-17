fd7ae6dbb391a4f59f5405a5c6cd2487597ede5d
kishan-dwivedi <93940049+kishan-dwivedi@users.noreply.github.com>
2022-08-18 00:30:22 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
-import os 
 print("bro you sucks ");
 
 
 bro = int(input("please tell me your age ? \n"))
 
-if bro<18:
+while (bro<18):
   print("see you kido")
+else:
+    age = input("sir you age is like an adult, are you adult? \n")
+    if age == "yes":
+        print("see you sir")
+    else:
+        print("kido")
