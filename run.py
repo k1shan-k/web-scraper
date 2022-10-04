235609704996f82aac781d00fab12b294073d71c
k1shan <93940049+k1shan-k@users.noreply.github.com>
2022-10-05 01:22:10 +0530

    Update run.py

--- run.py
+++ run.py
@@ -8,6 +8,8 @@
         * lxml
         * re
 """
+import sys
+import time
 from art import *
 from lxml import html
 import re
@@ -81,4 +83,14 @@ if T :
 
             return result	
     except Exception as e:
-            print("Error by machine")
+            print("Loading:")
+
+
+#animation = ["10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"]
+            animation = ["[■□□□□□□□□□]","[■■□□□□□□□□]", "[■■■□□□□□□□]", "[■■■■□□□□□□]", "[■■■■■□□□□□]", "[■■■■■■□□□□]", "[■■■■■■■□□□]", "[■■■■■■■■□□]", "[■■■■■■■■■□]", "[■■■■■■■■■■]"]
+
+            for i in range(len(animation)):
+                time.sleep(0.2)
+                sys.stdout.write("\r" + animation[i % len(animation)])
+                sys.stdout.flush()
+                print("\n")
